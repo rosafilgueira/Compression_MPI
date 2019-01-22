ff853ae9a3a589e49de1213c8628958b7993aaba
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-01-22 17:23:56 +0000

    better compile

--- PRACTICAL.5.0/compile-lzo.sh
+++ PRACTICAL.5.0/compile-lzo.sh
@@ -1,6 +1,6 @@
 set -x
-DIRLZO2=/lustre/home/y15/rosaf/Compression_MPI/PRACTICAL.5.0/lzo-2.05/bin
-DIRLZO=/lustre/home/y15/rosaf/Compression_MPI/PRACTICAL.5.0/lzo-2.05
+DIRLZO2=$HOME/Compression_MPI/PRACTICAL.5.0/lzo-2.05/bin
+DIRLZO=$HOME/Compression_MPI/PRACTICAL.5.0/lzo-2.05
 gcc \
 -I${DIRLZO2}/include/lzo/ \
 -I${DIRLZO2}/include/ \
