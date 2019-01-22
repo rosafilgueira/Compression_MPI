c0baefd0e6553fc5b7cad5c136655ddb534b79e3
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-01-22 18:16:15 +0000

    run_qdr

--- PRACTICAL.5.0/MPIDoctor/run_qdr.sh
+++ PRACTICAL.5.0/MPIDoctor/run_qdr.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
-#$ -N latency_qdr
-#$ -cwd
-#$ -pe openib_smp12_qdr 24
-#$ -R y
-#$ -l h_rt=00:30:00
+#!/bin/bash
+#PBS -N latency_qdr
+#PBS -l select=24:ncpus=36
+#PBS -l place=scatter:excl
+#PBS -l walltime=00:30:00
+#PBS -A z04
 
-. /etc/profile.d/modules.sh
+module load mpt
+module load intel-mpi-17
+export OMP_NUM_THREADS=1
+cd $PBS_O_WORKDIR
 
-module load mpich2/2.1.5 
+mpirun -np 24 ./latency
 
-mpirun -np $NSLOTS ./latency
